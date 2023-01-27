88304baedad069657feaedf38ceb1b701e6c050e
tunguyen2412 <tunguyen.080701@gmail.com>
2023-01-28 03:01:11 +0900

    main

--- WebsiteSellClothes/src/main/java/com/vti/controller/ImageController.java
+++ WebsiteSellClothes/src/main/java/com/vti/controller/ImageController.java
@@ -1,15 +1,23 @@
 package com.vti.controller;
 
+import com.vti.dto.CatalogDTO;
 import com.vti.dto.ImageDTO;
+import com.vti.entity.Catalog;
 import com.vti.entity.Image;
+import com.vti.form.creating.CatalogFormForCreating;
 import com.vti.form.creating.ImageFormForCreating;
+import com.vti.form.updating.CatalogFormForUpdating;
+import com.vti.service.implement.ICatalogService;
 import com.vti.service.implement.IImageService;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin("*")
 @RestController
@@ -36,7 +44,7 @@ public class ImageController {
         return new ResponseEntity<>(imageDTO, HttpStatus.OK);
     }
 
-    @PostMapping()
+    @PostMapping(value = "/create")
     public ResponseEntity<?> createImage(@RequestBody ImageFormForCreating form) {
         Image image = imageService.createImage(form);
         ImageDTO imageDTO = modelMapper.map(image, ImageDTO.class);
